87f0eb625e849e33bec8c15f28dd48c0ea49bfc4
Steven Christian Susanto <nicenicenice345@gmail.com>
2024-03-15 18:16:43 +0700

    j4p2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,13 +9,29 @@ class UserController extends Controller
     {
         $user = UserModel::firstorCreate(
             [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
+                'username' => 'manager55',
+                'nama' => 'Manager55',
                 'password' => Hash::make('12345'),
                 'level_id' => 2
             ],
         );
-        $user -> save();
-        return view('user', ['data' => $user]);
+
+        $user -> username = 'manager56';
+       
+        $user->isDirty();
+        $user->isDirty('username');
+        $user->isDirty('nama');
+        $user->isDirty(['nama', 'username']);
+
+        $user->isClean();
+        $user->isClean('username');
+        $user->isClean('nama');
+        $user->isClean(['nama', 'username']);
+
+        $user->save();
+
+        $user->isDirty();
+        $user->isClean();
+        dd($user->isDirty());
     }
 }
