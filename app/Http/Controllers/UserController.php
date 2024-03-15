f05cef17fa324da223dbe4acddc009748ae2229d
Steven Christian Susanto <nicenicenice345@gmail.com>
2024-03-15 14:18:48 +0700

    j4p2.4.5.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,50 +1,21 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Models\Usermodel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-
-
 class UserController extends Controller
 {
     public function index()
     {
-        // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
-        // ];
-        // UserModel::insert($data);
-
-        // $data = [
-        //     'nama' => 'Pelanggan Pertama',
-        // ];
-        // UserModel::where('username','customer-1')->update($data);
-
-        // $user = UserModel::all();
-        // return view('user', ['data' => $user]);
-
-        // $data = [
-        //         'level_id' => 2,
-        //         'username' => 'manager_tiga',
-        //         'nama' => 'Manager 3',
-        //         'password' => Hash::make('12345')
-        //     ];
-        //     UserModel::insert($data);
-
-        //     $user = UserModel::all();
-            $user = UserModel::firstOrCreate(
-                [
-                    'username' => 'manager33',
-                    'nama' => 'Manager Tiga Tiga',
-                    'password' => Hash::make('12345'),
-                    'level_id' => 2
-                ],
-            );
-            $user->save();
-            return view('user', ['data' => $user]);
+        $user = UserModel::firstorCreate(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user -> save();
+        return view('user', ['data' => $user]);
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,14 +4,14 @@
     </head>
     <body>
         <h1>Data User</h1>
-        <table>
+        <table border="1" cellpadding="2" cellspacing="0">
             <tr>
                 <th>ID</th>
                 <th>Username</th>
                 <th>Nama</th>
                 <th>ID Level Pengguna</th>
             </tr>
-            
+
             <tr>
                 <td>{{ $data->user_id }}</td>
                 <td>{{ $data->username }}</td>
