ec335258b60abd1ee40ea291665aa49c008504e3
Steven Christian Susanto <nicenicenice345@gmail.com>
2024-03-15 19:45:37 +0700

    j4p2.6

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -10,14 +10,17 @@
                 <th>Username</th>
                 <th>Nama</th>
                 <th>ID Level Pengguna</th>
+                <th>Aksi</th>
             </tr>
-
+            @foreach ($data as $d)
             <tr>
-                <td>{{ $data->user_id }}</td>
-                <td>{{ $data->username }}</td>
-                <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_id }}</td>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+                <td><a href={{route('/user/ubah',$d->user_id)}}>ubah</a> | <a href={{route('/user/hapus',$d->user_id)}}>hapus</a></td>
             </tr>
+            @endforeach
         </table>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,7 @@ Route::get('/', function () {
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
+
+Route::get('/user/tambah', [UserController::class, 'tambah'])->name('/user/tambah');
+Route::get('/user/ubah/{id}', [UserController::class,  'ubah'])->name('/user/ubah');
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus'])->name('/user/hapus');
